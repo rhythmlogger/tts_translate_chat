b24647a3a7483c7fd47c2899c1cc4913cfd205e6
rhythmlogger <rhythmlogger@naver.com>
2023-05-20 21:21:16 +0900

    update

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import os
 import time
 import win32com.client  # pywin32
-
+import pyautogui
 # github.com/rhythmlogger/tts
 folder_path = os.path.dirname(os.path.abspath(__file__)) + "\\log\\"
 speaker = win32com.client.Dispatch('SAPI.SpVoice')
@@ -20,6 +20,24 @@ def loop_test():
                     contents = f.read()
                     print(file)
                     print(contents)
+                    if (contents == '!++'):
+                        pyautogui.keyDown('2')
+                        pyautogui.keyUp('2')
+                        print(2)
+                    elif (contents == '!--'):
+                        pyautogui.keyDown('1')
+                        pyautogui.keyUp('1')
+                        print(1)
+                    elif (contents == '!0.5'):
+                        pyautogui.keyDown('3')
+                        pyautogui.keyUp('3')
+                        print(3)
+
+                    elif (contents == '!2'):
+                        pyautogui.keyDown('4')
+                        pyautogui.keyUp('4')
+                        print(4)
+
                     speaker.Speak(contents)
                     f.close()
                     os.remove(
